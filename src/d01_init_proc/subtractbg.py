7c021938fcb79f475599cc26eec228831a094a26
Kathryn Wu <wukathryn@gmail.com>
2024-02-19 13:57:33 -0800

    modified subtractbg workflow to preserve unprocessed channels

--- src/d01_init_proc/subtractbg.py
+++ src/d01_init_proc/subtractbg.py
@@ -42,8 +42,8 @@ def crop_black_borders(img):
 def clip_upper_outliers(img, outlier_percentiles):
     size_c = img.shape[1]
     outlier_thresh = []
-    for c in range(size_c):
-        outlier_thresh.append(np.percentile(img[:, c, :, :, :], q=outlier_percentiles[c], method='higher', keepdims=False))
+    for ch in range(size_c):
+        outlier_thresh.append(np.percentile(img[:, ch, :, :, :], q=outlier_percentiles[ch], method='higher', keepdims=False))
     outlier_thresh = np.expand_dims(outlier_thresh, axis=(0, 2, 3, 4))
     img_outliers_clipped = np.clip(img, a_min=None, a_max=outlier_thresh)
     return img_outliers_clipped.astype(img.dtype)
@@ -58,9 +58,9 @@ def get_otsu_thresholds(img):
     otsu_thresholds = np.zeros([size_t, size_c, size_z])
 
     for t in range(size_t):
-        for c in range(size_c):
+        for ch in range(size_c):
             for z in range(size_z):
-                otsu_thresholds[t, c, z] = threshold_otsu(img[t, c, z, :], nbins=65536)
+                otsu_thresholds[t, ch, z] = threshold_otsu(img[t, ch, z, :], nbins=65536)
 
     return otsu_thresholds
 
@@ -106,38 +106,48 @@ def subtract_bg(imgpath, params_list, bs_thresh_d, ch_to_process=None, index=Non
     ome_metadata = img_file.ome_metadata
     basename = imgpath.name.split('.')[0]
 
-    if ch_to_process is not None:
-        img_chs = [orig_img[:, ch, np.newaxis, :, :, :] for ch in ch_to_process]
-        orig_img = np.concatenate(img_chs, axis=1)
+    # Check processing parameters
+    size_t, size_c = orig_img.shape[:2]
+    params_list = check_params(params_list, len(ch_to_process))
 
-    # Crop black borders
-    crop_orig = crop_black_borders(orig_img)
+    # Extract channels to be processed
+    img_chsubset = None
+    for ch in ch_to_process:
+        if img_chsubset is None:
+            img_chsubset = orig_img[:, ch, np.newaxis, :, :, :]
+        else:
+            img_chsubset = np.concatenate((img_chsubset, orig_img[:, ch, np.newaxis, :, :, :]), axis=1)
 
-    # Get parameters
-    [size_t, size_c] = orig_img.shape[0:2]
-    params_list = check_params(params_list, size_c)
+    # Preprocess image for OTSU thresholding by cropping black borders
+    img_preprocessed = crop_black_borders(img_chsubset)
 
+    # Apply processing parameters
     for p, params in enumerate(params_list):
         outlier_percentiles = params['outlier_percentiles']
         sigmas_smoothing = params['sigmas_smoothing']
 
-        img_preprocessed = clip_upper_outliers(crop_orig, outlier_percentiles)
-        for c in range(size_c):
-            img_preprocessed[:, c, :, :, :] = gaussian_filter(img_preprocessed[:, c, :, :, :], (0, 0, sigmas_smoothing[c], sigmas_smoothing[c]))
+        # Continue to reprocess image for OTSU thresholding via clipping and smoothing
+        img_preprocessed = clip_upper_outliers(img_preprocessed, outlier_percentiles)
+        for ch in range(img_preprocessed.shape[1]):
+            img_preprocessed[:, ch, :, :, :] = gaussian_filter(img_preprocessed[:, ch, :, :, :], (0, 0, sigmas_smoothing[ch], sigmas_smoothing[ch]))
 
+        # Use OTSU to obtain background thresholds from pre-processed image
         otsu_thresholds = get_otsu_thresholds(img_preprocessed)
 
-        bg_sb_img = np.zeros_like(orig_img)
-        for c in range(size_c):
-            bg_sb_img[:, c, :, :, :] = gaussian_filter(orig_img[:, c, :, :, :], (0, 0, sigmas_smoothing[c], sigmas_smoothing[c]))
-        bg_sb_img = subtract_background(bg_sb_img, otsu_thresholds)
+        # Smooth the original image and subtract the background thresholds
+        bg_sb_chsubset = img_chsubset
+        for ch in range(bg_sb_chsubset.shape[0]):
+            bg_sb_chsubset[:, ch, :, :, :] = gaussian_filter(bg_sb_chsubset[:, ch, :, :, :], (0, 0, sigmas_smoothing[ch], sigmas_smoothing[ch]))
+        bg_sb_chsubset = subtract_background(bg_sb_chsubset, otsu_thresholds)
 
         # Save background subtracted image
-        combined_img = np.concatenate((bg_sb_img, crop_orig), axis=1)
+        bg_sb_img = orig_img
+        for i, c in enumerate(ch_to_process):
+            bg_sb_img[:, c, :, :, :] = bg_sb_chsubset[:, i, :, :, :]
         bgsub_imgname = f'{basename}_{bgsub_time}_p{p}'
-        bs_ome_metadata = construct_ome_metadata(combined_img, img_file.physical_pixel_sizes)
-        OmeTiffWriter.save(combined_img, bg_sub_dirpath / (bgsub_imgname + '.ome.tif'), ome_xml=bs_ome_metadata)
-        fig = vis_and_rescale.generate_subtractbg_fig(orig_img, bg_sb_img, bgsub_imgname, params,
+        bs_ome_metadata = construct_ome_metadata(bg_sb_img, img_file.physical_pixel_sizes)
+        OmeTiffWriter.save(bg_sb_img, bg_sub_dirpath / (bgsub_imgname + '.ome.tif'), ome_xml=bs_ome_metadata)
+        fig = vis_and_rescale.generate_subtractbg_fig(img_chsubset, bg_sb_chsubset, bgsub_imgname, params,
                                                       target_perc_grayval=target_perc_grayval,
                                                       index=index)
         fig.savefig(bg_sub_fig_dirpath / (bgsub_imgname + '.png'))
@@ -153,7 +163,7 @@ def subtract_bg(imgpath, params_list, bs_thresh_d, ch_to_process=None, index=Non
         for i, ch in enumerate(ch_to_process):
             bs_thresh_d[f'outlier perc ch{ch}'].extend([outlier_percentiles[i] * size_t])
             bs_thresh_d[f'sigma smoothing ch{ch}'].extend([sigmas_smoothing[i] * size_t])
-            thresh_ch = list(np.atleast_1d(otsu_thresholds[:, c, 0].squeeze()))
+            thresh_ch = list(np.atleast_1d(otsu_thresholds[:, i, 0].squeeze()))
             bs_thresh_d[f'thresh ch{ch}'].extend(thresh_ch)
 
     return bs_thresh_d
@@ -167,7 +177,19 @@ def get_params_df(bs_thresh_df, params_keys):
 
 
 def batch_subtract_bg(input_dirpath, params_list, ch_to_process, subset=None, target_perc_grayval=30, savefreq=5):
+    '''
+    Performs background subtraction on each image within a given directory
 
+    Parameters:
+        input_dirpath (str): full path for the image-containing directory
+        params_list (list or dict): list of dictionaries or single dictionary containing parameters for smoothing and clipping
+        ch_to_process (list): list of the channels to subtract background from
+        subset (list): list containing the indices of images to be processed
+        target_perc_grayval (int): target range out of all possible grayvalues represented within the image (used for scaling)
+        savefreq (int): determines the number of images processed before updating the output spreadsheet
+
+    Returns:
+    '''
     # Get input dirpath, create a directory for bg subtracted images
     input_dirpath = Path(input_dirpath)
     assert input_dirpath.exists(), "Please input a valid directory path"
@@ -205,12 +227,12 @@ def batch_subtract_bg(input_dirpath, params_list, ch_to_process, subset=None, ta
     bgsub_time = datetime.now().strftime("%Y%m%d-%p%I%M")
 
     ch_to_process = list(ch_to_process)
-    for c in ch_to_process:
-        bs_thresh_d[f'outlier perc ch{c}'] = []
-        params_keys.append(f'outlier perc ch{c}')
-        bs_thresh_d[f'sigma smoothing ch{c}'] = []
-        params_keys.append(f'sigma smoothing ch{c}')
-        bs_thresh_d[f'thresh ch{c}'] = []
+    for ch in ch_to_process:
+        bs_thresh_d[f'outlier perc ch{ch}'] = []
+        params_keys.append(f'outlier perc ch{ch}')
+        bs_thresh_d[f'sigma smoothing ch{ch}'] = []
+        params_keys.append(f'sigma smoothing ch{ch}')
+        bs_thresh_d[f'thresh ch{ch}'] = []
 
     # Loop through and process each image
     for i, imgpath in enumerate(imgpaths):
@@ -236,7 +258,7 @@ def batch_subtract_bg(input_dirpath, params_list, ch_to_process, subset=None, ta
     bs_thresh_df = pd.concat([bs_thresh_df_prev, bs_thresh_df], ignore_index=True)
     bs_thresh_df.to_csv(bs_thresh_path, index=False)
 
-    print(f'Done!')
+    print(f'Finished processing directory!')
 
 
 def check_params(params_list, size_c):
@@ -247,6 +269,7 @@ def check_params(params_list, size_c):
     # Check listed params within the params list
     for params in params_list:
         for key, value in params.items():
+            # if the params are not in list-format, convert it to a list
             if isinstance(value, (int, float, complex)):
                 value = [value]
             assert isinstance(value, list), \
@@ -257,5 +280,5 @@ def check_params(params_list, size_c):
                 f'For {key}, please list either 1 value to apply to all channels' \
                 f' or 1 value for each channel ({size_c} total)'
             params[key] = value
-    return params_li
+    return params_list
 
